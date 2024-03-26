1811e1016d26dbc670c6e85157b7b095d939747c
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-03-25 23:23:19 -0500

    oh no

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
